c96bf797336a1a35b2d90bdf9b8e55266a532528
user2 <veronicadwiyanti11@gmail.com>
2023-12-15 01:23:28 +0700

    klik img logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({status}) => {
-    if(status=='logout'){
-        return(
-            <header>
-                <img src={process.env.PUBLIC_URL + '/logo-skinmates.png'} alt="Skinmates Logo" className='img-logo'/>
-
-                <input type="text" placeholder="Find Products..." className='search-bar'/>
-
-                <div className='button-and-notif'>
-                    <button className='pink-button'><a href='/login'>Login / Signup</a></button>
-                    {/* <button className='icon-button'>
-                        <img src={process.env.PUBLIC_URL + '/notification.png'} alt="Notification Icon" className='icon'/>
-                    </button> */}
-                </div> 
-            </header>
-        );
+const Navbar = ({ status }) => {
+    if (status === 'logout') {
+      return (
+        <header>
+          <Link to="/">
+            <img src={process.env.PUBLIC_URL + '/logo-skinmates.png'} alt="Skinmates Logo" className='img-logo' />
+          </Link>
+  
+          <input type="text" placeholder="Find Products..." className='search-bar' />
+  
+          <div className='button-and-notif'>
+            <button className='pink-button'><a href='/login'>Login / Signup</a></button>
+          </div>
+        </header>
+      );
+    } else {
+      return (
+        <header>
+          <Link to="/">
+            <img src={process.env.PUBLIC_URL + '/logo-skinmates.png'} alt="Skinmates Logo" className='img-logo' />
+          </Link>
+  
+          <input type="text" placeholder="Find Products..." className='search-bar' />
+  
+          <div className='button-and-notif'>
+            <button className='pink-button'><a href='/profile'>Profile</a></button>
+          </div>
+        </header>
+      );
     }
-    else {
-        return(
-            <header>
-                <img src={process.env.PUBLIC_URL + '/logo-skinmates.png'} alt="Skinmates Logo" className='img-logo'/>
-
-                <input type="text" placeholder="Find Products..." className='search-bar'/>
-
-                <div className='button-and-notif'>
-                    <button className='pink-button'><a href='/profile'>Profile</a></button>
-                </div> 
-            </header>
-        );
-    }
-};
-
-export default Navbar;
+  };
+  
+  export default Navbar;
